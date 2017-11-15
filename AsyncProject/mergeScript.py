76f881e89d5da7ff18cbab1c8d64c6baf9c80a7d
saimadanmohan <saimadanmohan@hotmail.com>
2017-11-15 02:55:58 -0500

    phase 2

--- AsyncProject/mergeScript.py
+++ AsyncProject/mergeScript.py
@@ -1,11 +1,31 @@
 from subprocess import call
+import sys
 #open('BCR.da', 'w').close()
 #subprocess.check_output(['ls','-l']) #all that is technically needed...
-filenames = ['ByzantineReplica.da','ByzantineHelper.da', 'ByzantineOlympus.da', 'ByzantineClient.da']
-with open('BCR.da', 'w') as outfile:
-    for fname in filenames:
-        with open(fname) as infile:
-            outfile.write(infile.read())
 
-call(["/Users/sai/Courses/Async/pyDistAlgo-1.0.9/bin/dar", "./BCR.da"])
-#print(subprocess.check_output(['/Users/sai/Courses/Async/pyDistAlgo-1.0.9/bin/dar','./BCR.da']))
+param = None
+if(len(sys.argv) > 1):
+	param = sys.argv[1]
+
+if param is not None and (param == "r" or param == "rm"):
+	call('rm ./*.log', shell=True)
+elif param == "run":
+	call('/Users/sai/packages/pyDistAlgo-1.0.10/bin/dar --message-buffer-size 10000 ./BCR.da', shell=True)
+elif param == "merge" or param =="m":
+	filenames = ['ByzantineReplica.da','ByzantineHelper.da', 'ByzantineOlympus.da', 'ByzantineClient.da','ByzantineParentProcess.da']
+	with open('BCR.da', 'w') as outfile:
+	    for fname in filenames:
+	        with open(fname) as infile:
+	            outfile.write(infile.read())
+elif param == None:
+	call('rm ./*.log', shell=True)
+
+	filenames = ['ByzantineReplica.da','ByzantineHelper.da', 'ByzantineOlympus.da', 'ByzantineClient.da','ByzantineParentProcess.da']
+	with open('BCR.da', 'w') as outfile:
+	    for fname in filenames:
+	        with open(fname) as infile:
+	            outfile.write(infile.read())
+
+	
+	call('/Users/sai/packages/pyDistAlgo-1.0.10/bin/dar --message-buffer-size 10000 ./BCR.da', shell=True)
+	#call(["/Users/sai/packages/pyDistAlgo-1.0.10/bin/dar"," --message-buffer-size 5000 " ,"./BCR.da"], shell=True)
